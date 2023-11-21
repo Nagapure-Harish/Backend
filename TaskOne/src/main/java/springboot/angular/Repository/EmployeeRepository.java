e7469aaa240c5c1264bf08fcb62b87b1d693f087
Nagapure-Harish <149144160+Nagapure-Harish@users.noreply.github.com>
2023-11-21 13:05:31 +0530

    Add files via upload
    success

---
+++ TaskOne/src/main/java/springboot/angular/Repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package springboot.angular.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import springboot.angular.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
